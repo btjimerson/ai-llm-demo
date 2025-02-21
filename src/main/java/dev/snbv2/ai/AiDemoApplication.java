aecd9e1693941623e8a59daa227f89705521e5e4
Brian Jimerson <bjimerson@gmail.com>
2025-02-21 13:50:28 -0500

    Remove embeddings

--- src/main/java/dev/snbv2/ai/AiDemoApplication.java
+++ src/main/java/dev/snbv2/ai/AiDemoApplication.java
@@ -1,65 +1,23 @@
 package dev.snbv2.ai;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.ai.document.DocumentReader;
-import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
-import org.springframework.ai.transformer.splitter.TextSplitter;
-import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 /**
  * Entry point for the application
+ * 
+ * @author Brian Jimerson
  */
 @SpringBootApplication
-public class AiDemoApplication implements CommandLineRunner {
-
-	private static final Log log = LogFactory.getLog(AiDemoApplication.class);
-
-	@Autowired
-	VectorStore vectorStore;
-
-	@Value("${llm.use-embeddings}")
-	String useEmbeddings;
+public class AiDemoApplication {
 
 	/**
 	 * Main entry point for the Spring Boot application
+	 * 
 	 * @param args Any command line arguments passed
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AiDemoApplication.class, args);
 	}
-
-
-	/**
-	 * CommandLineRunner method to add embeddings to the vector store if <code>useEmbeddings</code> is true.
-	 */
-	@Override
-    public void run(String... args) throws Exception {
-		
-		if (!Boolean.valueOf(useEmbeddings)) return;
-
-		PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
-		Resource[] resources = resourceResolver.getResources("classpath:/docs/*.pdf");
-
-        TextSplitter textSplitter = new TokenTextSplitter();
-
-        DocumentReader pdfReader;
-		
-		for (Resource resource : resources) {
-			pdfReader = new PagePdfDocumentReader(resource);
-			vectorStore.add(textSplitter.apply(pdfReader.get()));
-		}
-
-        log.info(String.format("Loaded vector database with embeddings from %d files.", resources.length));
-
-    }
 
 }
--- src/main/java/dev/snbv2/ai/AiDemoConfiguration.java
+++ src/main/java/dev/snbv2/ai/AiDemoConfiguration.java
@@ -1,9 +1,6 @@
 package dev.snbv2.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer;
 import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -13,51 +10,47 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestClient;
 
 /**
- * Configuration class for the application
+ * Configuration class for the application.
+ * 
+ * @author Brian Jimerson
  */
 @Configuration
 public class AiDemoConfiguration {
-    
-    @Value("${llm.use-embeddings}")
-	String useEmbeddings;
 
     /**
      * Creates a RestClient (wrapped by a ChatClient) with specific client
      * interceptors attached.
+     * 
      * @param configurer The Rest Client Configurer to use
      * @return A RestClient Builder with the specific interceptors.
      */
-	@Bean
-	@Scope("prototype")
-	RestClient.Builder restClientBuilder(RestClientBuilderConfigurer configurer) {
-		RestClient.Builder builder = 
-			RestClient
+    @Bean
+    @Scope("prototype")
+    RestClient.Builder restClientBuilder(RestClientBuilderConfigurer configurer) {
+        RestClient.Builder builder = RestClient
                 .builder()
                 .requestFactory(ClientHttpRequestFactoryBuilder.simple().build())
                 .requestInterceptor(new ClientAuthorizationInterceptor())
-			    .requestInterceptor(new ChatClientLoggingInterceptor());
-		
-		return configurer.configure(builder);
+                .requestInterceptor(new ChatClientLoggingInterceptor());
 
-	}
+        return configurer.configure(builder);
+
+    }
 
     /**
      * Creates a ChatClient with the appropriate Advisors.
+     * 
      * @param chatClientBuilder The ChatClient Builder to use
-     * @param vectorStore The VectorStore to use for embeddings
      * @return A ChatClient with the appropriate Advisors.
      */
     @Bean
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
-        if (Boolean.valueOf(useEmbeddings)) {
-            return chatClientBuilder.defaultAdvisors(new ErrorHandlingAdvisor(), new QuestionAnswerAdvisor(vectorStore)).build();
-        } else {
-            return chatClientBuilder.defaultAdvisors(new ErrorHandlingAdvisor()).build();
-        }
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.defaultAdvisors(new ErrorHandlingAdvisor()).build();
     }
 
     /**
      * Creates a FilterRegistrationBean for an AuthorizationFilter.
+     * 
      * @return A FilterRegistrationBean for an AuthorizationFilter.
      */
     @Bean
